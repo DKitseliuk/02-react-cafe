3de6ae1158dc7b10149c8397fe0d8fb61d2491e9
Dmytro Kitseliuk <dima_kitseliuk@ukr.net>
2025-11-20 18:38:31 +0200

    fix app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
-import Notification from '../Notification1/Notification';
+import Notification from '../Notification/Notification';
 import type { Votes, VoteType } from '../../types/votes';
 
 import css from './App.module.css';
